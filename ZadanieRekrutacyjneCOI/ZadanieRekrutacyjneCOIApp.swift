1cdd517f1bd93d213f0d0af788cb947a293306d2
Filip Skup <filipskup231@gmail.com>
2026-04-13 13:20:39 +0200

    Initial Commit

---
+++ ZadanieRekrutacyjneCOI/ZadanieRekrutacyjneCOIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZadanieRekrutacyjneCOIApp.swift
+//  ZadanieRekrutacyjneCOI
+//
+//  Created by Filip Skup on 13/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct ZadanieRekrutacyjneCOIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ZadanieRekrutacyjneCOITests/ZadanieRekrutacyjneCOITests.swift
@@ -0,0 +1,19 @@
+//
+//  ZadanieRekrutacyjneCOITests.swift
+//  ZadanieRekrutacyjneCOITests
+//
+//  Created by Filip Skup on 13/04/2026.
+//
+
+import Testing
+@testable import ZadanieRekrutacyjneCOI
+
+struct ZadanieRekrutacyjneCOITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
